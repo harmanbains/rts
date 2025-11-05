083bae4fa8ce3bf5eeebfc6fedd38d984661824e
Harman Bains <bains.hmn@gmail.com>
2025-11-04 16:16:08 -0800

    event types

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from "react-dom/client";
-import UserSearch from "./state/UserSearch";
+import ReactDOM from 'react-dom/client';
+import EventComponent from './events/EventComponent';
 
-const el = document.getElementById("root");
+const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
   return (
     <div>
-      <UserSearch />
+      <EventComponent />
     </div>
   );
 };
